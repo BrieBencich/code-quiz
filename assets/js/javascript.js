199873e530eb835f7cce3d6f9f5854880477eb29
Brie Bencich <bbencich@hotmail.com>
2020-09-27 21:36:34 +0300

    questions wrtitten in Java

---
+++ assets/js/javascript.js
@@ -0,0 +1,26 @@
+// question choices / answers
+var questions = [
+    {
+        title: "If you type the following code in the console window, what will you get ? : 3>2>1===false;",
+        choices: [true, false],
+        answer: "true" },
+
+    { 
+        title: "JavaScript is a _____ -side programming language.",
+        choices: [client, server, both, none],
+        answer: "both"},
+    {
+        title: "How do you find the minimum of x and y using JavaScript?",
+        choices: [ min(x,y), Math.min(x,y), min(xy), ],
+        answer: "Math.min(x,y)"},
+
+    {
+        title: "Which JavaScript label catches all the values, except for the ones specified?",
+        choices: [catch, label, try, default],
+        answer: "default"
+    }
+    
+    
+];
+    
+  
